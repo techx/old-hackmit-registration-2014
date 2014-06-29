4ee1ed175172760b5d453016a4fa1fd50612ac3f
Edwin Zhang <ezhang@groupon.com>
2014-06-29 14:36:26 -0500

    Animation to team, get data

---
+++ reg/static/js/team.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.team')
+    .transition('fade in');
+
+  var $teamCode = $('#team-name'),
+      $teamCode = $('#team-code'),
+      $formTeamName = $('.ui.team.name.form'),
+      $formTeamCode = $('.ui.team.code.form');
+
+});
