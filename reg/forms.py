a989570bb4ad2c6433fb7efddfb0d4035e15d7bd
Aneesh Agrawal <aneeshusa@gmail.com>
2014-06-29 11:38:29 -0700

    Add form validation for email fields for login and register.

--- reg/forms.py
+++ reg/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import Form
-from flask_wtf.html5 import EmailField
 from wtforms import TextField
+from wtforms.validators import DataRequired, Email, Regexp
 
 class RegistrationForm(Form):
     #type = TextField() Need to change name of field
-    email = TextField()
+    email = TextField(validators=[DataRequired(), Email(), Regexp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.edu$")])
     hashedPassword = TextField()
 
 class LoginForm(Form):
-    email = TextField(validators=[])
+    email = TextField(validators=[DataRequired(), Email()])
     hashedPassword = TextField()
