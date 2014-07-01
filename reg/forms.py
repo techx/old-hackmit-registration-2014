69cf1c9c1253dc05107423fff0e093b3c4ea80fd
Aneesh Agrawal <aneeshusa@gmail.com>
2014-06-30 18:18:10 -0700

    Change DataRequired() to InputRequired() in ResetForm

--- reg/forms.py
+++ reg/forms.py
@@ -21,6 +21,6 @@ class LotteryForm(Form):
     inviteCode = TextField(validators=[Optional()])
 
 class ResetForm(Form):
-    email = TextField(validators=[DataRequired(), Email(), Regexp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.edu$")])
-    oldPassword = TextField(validators=[DataRequired(), Length(min=62, max=62), Regexp("^[a-z0-9]+$")])
-    newPassword = TextField(validators=[DataRequired(), Length(min=62, max=62), Regexp("^[a-z0-9]+$")])
+    email = TextField(validators=[InputRequired(), Email(), Regexp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.edu$")])
+    oldPassword = TextField(validators=[InputRequired(), Length(min=62, max=62), Regexp("^[a-z0-9]+$")])
+    newPassword = TextField(validators=[InputRequired(), Length(min=62, max=62), Regexp("^[a-z0-9]+$")])
