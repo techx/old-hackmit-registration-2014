5b2069f675db835e524608f13b1e14870cb53bf2
Edwin Zhang <ezhang@groupon.com>
2014-07-01 12:33:50 -0500

    enable scrolling on mobile

--- reg/static/js/index.js
+++ reg/static/js/index.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
-  skrollr.init({
-    forceHeight: false
-  });
+  if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+    skrollr.init({
+      forceHeight: false
+    });
+  }
 
   $('#main')
     .transition('fade up in', 2000);
