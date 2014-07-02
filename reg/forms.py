6d8949556e5fca146e83463c45fd954d23c34be0
Aneesh Agrawal <aneeshusa@gmail.com>
2014-07-01 21:37:26 -0700

    Remove .edu validation from the RegistrationForm.

--- reg/forms.py
+++ reg/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import InputRequired, Optional, Length, Email, Regexp, A
 
 class RegistrationForm(Form):
     role = TextField(validators=[InputRequired(), Regexp("^[a-z]+$")])
-    email = TextField(validators=[InputRequired(), Email(), Regexp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.edu$")])
+    email = TextField(validators=[InputRequired(), Email(), Regexp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+$")])
     hashedPassword = TextField(validators=[InputRequired(), Length(min=62, max=62), Regexp("^[a-z0-9]+$")])
 
 class LoginForm(Form):
