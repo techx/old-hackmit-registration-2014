ee091f1340d27f2effbb17f95470b0d8bd6feb83
Edwin Zhang <ezhang@groupon.com>
2014-07-02 22:27:20 -0500

    remove SERVER_NAME from dev config

--- reg/config/dev.py
+++ reg/config/dev.py
@@ -3,7 +3,6 @@ class DevelopmentConfig(base.Config):
     PORT = 5001
     DEBUG = True
     SECRET_KEY = "THIS DOESN'T MATTER!"
-    SERVER_NAME = 'localhost:5001'
     SQLALCHEMY_BINDS = {
         'local':   'sqlite:///dev-local.db',
         'central': 'sqlite:///dev-central.db'
