8eba0f590d0468bd9b1a0a3489973433bc9a7028
Aneesh Agrawal <aneeshusa@gmail.com>
2014-08-03 14:01:07 -0700

    Fix S3 uploading: many changes.
    -Turn the file upload into a proper button.
    -Integrate with Semantic UI for prettier button.
    -Provide prettier upload done action
    -Provide button [form] template to include in forms
    -Allow for multiple uses of button (to upload >1 file / form)
    -Integrate permissions support to restrict access
    -Namespace uploads to restrict damange do-able by users. (They can arbitrarily hit the policy endpoints they are authenticated for, so jail them into their own folder.)
    -Provide help functions in util to register new customized policy endpoints
    -Increase security of form upload with exact key matching
    -Make util a proper blueprint in order to enable serving content
    -Update CSS with new attributes to handle invisible elements and completed buttons

--- website/application/util/__init__.py
+++ website/application/util/__init__.py
@@ -1,1 +1,9 @@
-from toposort import toposort, toposorted
+from flask import Blueprint
+
+blueprint_name = 'util'
+
+bp = Blueprint(blueprint_name, __name__, static_folder='static', static_url_path='/' + blueprint_name + '/static', template_folder='templates')
+
+# Util module only: exported functions
+from .toposort import toposort, toposorted
+from .s3_upload import s3_config, register_policy_route
