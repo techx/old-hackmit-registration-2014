5b616459dcc48b8c7abad42225a1da6c8d0a0a8c
Aneesh Agrawal <aneeshusa@gmail.com>
2014-08-04 20:46:40 -0700

    Make the github field optional in admit/forms.py.

--- website/application/admit/forms.py
+++ website/application/admit/forms.py
@@ -9,6 +9,6 @@ class ConfirmationForm(AttendeeForm):
     diet = TextField(validators=[InputRequired(), Length(max=6), AnyOf(['none', 'veg', 'vegan', 'kosher', 'halal', 'other'])])
     resumeOptOut = TextField()
     resume = TextField()
-    github = TextField(validators=[InputRequired(), Length(min=1,max=39), Regexp("^[a-zA-Z0-9][-a-zA-Z0-9]{0,38}")])
+    github = TextField(validators=[Optional(), Length(min=1,max=39), Regexp("^[a-zA-Z0-9][-a-zA-Z0-9]{0,38}")])
     travel = TextField()
     likelihood = TextField(validators=[InputRequired(), Length(min=4, max=10), AnyOf(['li_maybe', 'li_likely', 'li_yes'])])
